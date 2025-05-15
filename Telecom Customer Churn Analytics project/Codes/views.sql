fdc501e49bf738c62a1e53fb6abd5ff671072f82
PradeepYenni <52691562+pradeepkumaryenni@users.noreply.github.com>
2025-05-15 12:57:45 +0530

    Delete Telecom Customer Churn Analytics project directory

--- Telecom Customer Churn Analytics project/Codes/views.sql
+++
@@ -1,5 +0,0 @@
-create view v_chrundata as select * from [dbo].[Churn_Data]  where Customer_status in ('stayed','Churned')
-
-create view v_joindata as select * from [dbo].[Churn_Data]  where Customer_status in ('joined')
-
-select * from [dbo].[Churn_Data]
